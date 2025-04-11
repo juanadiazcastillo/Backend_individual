c1237fbc60a5a9c9f923278c8774bbb37a487213
Juana <diazcastillojuana2@gmail.com>
2025-04-10 22:15:16 -0600

    Cambios buscar

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {  obtenerProductos, obtenerProducto } from '../controllers/productos.controller.js';
+import {  obtenerProductos, obtenerProducto, registrarProducto } from '../controllers/productos.controller.js';
 
 const router = Router();
 
@@ -8,5 +8,8 @@ router.get('/productos', obtenerProductos);
 
 // Ruta para obtener un produto por su ID
 router.get('/producto/:id', obtenerProducto);
+
+// Ruta para registar todos los productos
+router.post('/registrarproducto', registrarProducto);
 
 export default router;
